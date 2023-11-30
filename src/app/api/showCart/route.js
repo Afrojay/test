304a3ce93670857ceb2b165fd2227c587eab37fd
Afrojay <b00135409@mytudublin.ie>
2023-11-30 17:35:46 +0000

    111

--- src/app/api/showCart/route.js
+++ src/app/api/showCart/route.js
@@ -1,15 +1,15 @@
-export async function REM(req, res) {
+export async function GET(req, res) {
     // Make a note we are on
     // the api. This goes to the console.
-    console.log("in the removeFromCart api page")
-    // get the values
-    // that were sent across to us.
-    const { searchParams } = new URL(req.url)
-    const pname = searchParams.get('pname')
-    console.log(pname);
+    console.log("in the api page")
     // =================================================
     const { MongoClient } = require('mongodb');
     const url = 'mongodb+srv://rojojojoman:jVXQ7sQiuGePvoMy@cluster0.reodhks.mongodb.net/?retryWrites=true&w=majority/';
+
+
+    // jVXQ7sQiuGePvoMy
+
+
 
     const client = new MongoClient(url);
     const dbName = 'app'; // database name
@@ -17,10 +17,9 @@ export async function REM(req, res) {
     console.log('Connected successfully to server');
     const db = client.db(dbName);
     const collection = db.collection('shopping_cart'); // collection name
-    var myobj = { pname: pname, username: "sample@test.com"};
-    const deleteResult = await collection.deleteOne(myobj);
+    const findResult = await collection.find({}).toArray();
+    console.log('Found documents =>', findResult);
     //==========================================================
     // at the end of the process we need to send something back.
-    return Response.json({ "data":"" + "removed" + ""})
+    return Response.json(findResult)
     }
-    
